06aa519304b7c5b0686ccb763ca96d610c51d9c4
GuoJing <soundbbg@gmail.com>
2012-11-26 16:34:31 +0800

    using fmdb

---
+++ Consts.h
@@ -0,0 +1,13 @@
+//
+//  Consts.h
+//  PasswordSaver
+//
+//  Created by GuoJing on 12-11-26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Consts : NSObject
+
+@end
---
+++ Consts.m
@@ -0,0 +1,13 @@
+//
+//  Consts.m
+//  PasswordSaver
+//
+//  Created by GuoJing on 12-11-26.
+//
+//
+
+#import "Consts.h"
+
+@implementation Consts
+
+@end
